a1b454b20e1149c5f94044279858ca67c0a15aeb
anastasiyakohut <152920842+anastasiyakohut@users.noreply.github.com>
2024-04-16 18:01:48 +0300

    js mod7

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(category => {
+    const categoryName = category.querySelector('h2').textContent;
+    const categoryItems = category.querySelectorAll('ul > li');
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Number of items: ${categoryItems.length}`);
+});
