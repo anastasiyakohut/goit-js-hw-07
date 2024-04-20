7c1cbdd04637947efb704132a9291443a6da43d9
anastasiyakohut <152920842+anastasiyakohut@users.noreply.github.com>
2024-04-20 13:05:18 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -4,18 +4,24 @@ const destroyBtn = document.querySelector('[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 function createBoxes(amount) {
+  boxesContainer.innerHTML = '';
+  const fragment = document.createDocumentFragment();
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
     const size = 30 + i * 10;
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
+    fragment.appendChild(box);
   }
+
+  boxesContainer.appendChild(fragment);
 }
 
 function destroyBoxes() {
